8faba822d68e05ab10f398d3272d00c41dd65047
TheJostler <josj@tegosec.com>
2025-02-05 18:43:06 +0000

    initial commit

---
+++ whatsapp.py
@@ -0,0 +1,106 @@
+import os, argparse, requests, configparser, json
+
+configdir = os.path.expanduser('~/.whatsapp_automation')
+version = "0.1 -- 2025 By Josjuar Lister"
+API_URL = ""  # Change if running API elsewhere
+
+def configure() -> configparser.ConfigParser:
+    #Create directory to store user data
+    if(not os.path.exists(f"{configdir}")):
+        print(f"Creating directory {configdir}")
+        os.makedirs(configdir)
+
+    config = configparser.ConfigParser()
+
+    if(not os.path.exists(f"{configdir}/config.cfg")):
+        default_config = f"""[Settings]
+                            chrome_user_data_dir={configdir}/chrome-data
+                            whatsapp_url=https://web.whatsapp.com
+                            port=5000
+                            log_path={configdir}/whatsapp_web_service.log"""
+        # Create configuration file
+        with open(f"{configdir}/config.cfg", 'w') as configfile:
+            configfile.write(default_config)
+        config.read_string(default_config)
+    else:
+        # Read configuration from cfg file
+        config.read(f"{configdir}/config.cfg")
+    return config
+
+def api_call(endpoint, params):
+    global API_URL
+    r = None
+    try:
+        r = requests.get(f"{API_URL}/{endpoint}", params=params)
+    except Exception as e:
+        print(f"An error occoured: {e}")
+    response = r.json()
+    if(response["status"] != "success"):
+        print("The WhatsApp Web service experienced a problem")
+        exit(1)
+    return response
+
+def read_chat(chat_name, n_read):
+    response = api_call("read", {"chat_name": chat_name, "n_read": n_read})
+    print(f"🔔 Messages from {chat_name}:")
+    for msg in response["messages"]:
+        print(f"📨 {msg}")
+
+def get_latest_unread_messages():
+    chats = api_call("last", [])
+
+    print(f"✅ Found {chats['chats found']} Chat's with unread messages")
+    if chats["chats found"] != 0:
+        print("🔔 Unread Chats:")
+        for c in chats["chats"]:
+            print(f"📨 {c['message_count']} Messages from: \"{c['chat_title']}\"")
+
+def send_message(chat_name, message):
+    response = api_call("send", {"chat_name": chat_name, "message": message})
+    print(f"✅ Message sent to {response['chat']}")
+
+def run(args):
+    # Read chat messages
+    if args.read:
+        chat_name = input("Which chat do you want to read? -- ") if args.read is True else args.read
+        read_chat(chat_name, args.number)
+        exit(0)
+   
+    # See unread messages
+    if args.last:
+        get_latest_unread_messages()
+        exit(0)
+    
+    if args.send:
+        if args.to is None:
+            to = input("Who would you like to send that to? -- ")
+        else:
+            to = args.to
+        send_message(to, args.send)
+        exit(0)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A CLI client for WhatsApp Web automation")
+    parser.add_argument("--version", action="version", version=f"%(prog)s {version}")
+    parser.add_argument("--send", help="Message to send")
+    parser.add_argument("--to", help="Contact name")
+    parser.add_argument("--last", help="Show latest unread messages", action="store_true")
+    parser.add_argument("--read", help="Read messages from a specified chat", nargs="?", const=True)
+    parser.add_argument("--number", "-n", help="How many messages to display from a chat", type=int, default=1)
+    args = parser.parse_args()
+
+    config = configure()
+    API_URL = f"http://localhost:{config.get('Settings', 'port')}/"
+
+    try:
+        requests.get(f"{API_URL}/version")
+    except:
+        print("Please start the web service API")
+
+    run(args)
+
+    # If no arguments are passed
+    to = input("Enter the contact name: ")
+    message = input("Enter your message: ")
+    send_message(to, message)
+
---
+++ whatsapp_web_service.py
@@ -0,0 +1,215 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from flask import Flask, request, jsonify
+import time, configparser, os, argparse
+
+configdir = os.path.expanduser('~/.whatsapp_automation')
+version = "whatsapp_web_service 0.1 -- 2025 By Josjuar Lister"
+
+driver: webdriver.Chrome = None
+app = Flask(__name__)
+logpath = ""
+logfile = None
+
+def logf(string):
+    log = f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {string}"
+    print(log)
+    logfile.write(log + '\n')
+
+def configure() -> configparser.ConfigParser:
+    #Create directory to store user data
+    if(not os.path.exists(f"{configdir}")):
+        logf("Creating directory /var/local/whatsapp_automation")
+        os.makedirs(os.path.expanduser('~/.whatsapp_automation'))
+
+    config = configparser.ConfigParser()
+
+    if(not os.path.exists(f"{configdir}/config.cfg")):
+        default_config = f"""[Settings]
+                            chrome_user_data_dir={configdir}/chrome-data
+                            whatsapp_url=https://web.whatsapp.com
+                            port=5000
+                            log_path={configdir}/whatsapp_web_service.log"""
+        # Create configuration file
+        with open(f"{configdir}/config.cfg", 'w') as configfile:
+            configfile.write(default_config)
+        config.read_string(default_config)
+    else:
+        # Read configuration from cfg file
+        config.read(f"{configdir}/config.cfg")
+    return config
+
+def getdriver(config: configparser.ConfigParser, headless=True):
+    # Extract configuration values
+    chrome_user_data_dir = config.get('Settings', 'chrome_user_data_dir', fallback=f"{configdir}/chrome-data")
+
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"--user-data-dir={chrome_user_data_dir}")  # Keeps session alive
+    if (headless):
+        options.add_argument("--headless=new")  # New headless mode
+        options.add_argument("--disable-gpu")  # Fixes rendering issues
+        options.add_argument("--window-size=1920,1080")  # Ensures proper page loading
+        options.add_argument("--no-sandbox")  # Prevents sandboxing issues
+        options.add_argument("--enable-webgl")  # Ensures WebRTC loads properly
+        options.add_argument("--use-fake-ui-for-media-stream")  # Stops camera popups
+
+        # Spoof user-agent to appear like a normal Chrome browser
+        options.add_argument(
+            "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/121.0.0.0 Safari/537.36"
+        )
+    
+    driver = webdriver.Chrome(options=options)
+    whatsapp_url = config.get('Settings', 'whatsapp_url', fallback='https://web.whatsapp.com/')
+
+    # Open WhatsApp Web
+    driver.get(f"{whatsapp_url}")
+    logf("🔗 Opening WhatsApp Web...")
+
+    try:
+        search_box = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@contenteditable='true']"))
+        )
+        logf("✅ Logged in!")
+    except:
+        logf("❌ Login failed! Search box not found.")
+        return jsonify({"status": "failure", "error": "Failed to load chat window"}), 500
+
+    return driver
+
+def get_chat(chat_name):
+    if not chat_name:
+        return jsonify({"status": "failure", "error": "Missing chat_name"}), 400
+    try:
+        search_box = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@contenteditable='true']"))
+        )
+    except:
+        logf("❌ failed! Search box not found.")
+        driver.quit()
+        return        
+            
+    # Search for the contact
+    logf(f"🔍 Searching for {chat_name}...")
+    search_box.click()
+    search_box.clear()
+    search_box.send_keys(chat_name)
+    time.sleep(1)
+    search_box.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+def clear_search():
+  try:
+    search_icon = WebDriverWait(driver, 30).until(
+      EC.presence_of_element_located((By.XPATH, "//span[@data-icon='search']"))
+    )
+    search_icon.click()
+    logf("🔍 Search box cleared.")
+  except Exception as e:
+    logf(f"❌ Failed to clear search box: {e}")
+
+@app.route('/send', methods=['GET'])
+def send():
+    global driver 
+    chat_name = request.args.get("chat_name")
+    message = request.args.get("message")
+    if not chat_name or not message:
+        return jsonify({"status": "failure", "error": "Missing chat_name or message"}), 400
+    
+    get_chat(chat_name)
+    message_lines = message.split("\n")
+
+    # Find the message box
+    logf(f"✅ Found {chat_name}")
+    message_box = driver.find_elements(By.XPATH, "//div[@contenteditable='true']")[-1]
+    message_box.click()
+    for line in message_lines:
+        message_box.send_keys(line)
+        message_box.send_keys(Keys.SHIFT, Keys.ENTER)  # New line without sending
+    message_box.send_keys(Keys.ENTER)  # Send final message
+
+    message_box.send_keys(Keys.ENTER)
+
+    logf(f"✅ Message sent to {chat_name}")
+    clear_search()
+    return jsonify({"status": "success", "chat": chat_name, "message": message})
+
+@app.route('/last', methods=['GET'])
+def get_last_unread_messages():
+    global driver
+    
+    if driver is None:
+        return jsonify({"error": "Selenium driver not initialized"}), 500
+    
+    # Find the unread messages
+    unread_messages = driver.find_elements(By.XPATH, "//span[contains(@aria-label, 'unread message')]")
+    n_chats = len(unread_messages)-1
+    logf(f"✅ Found {n_chats} Chat's with unread messages")
+
+    chats =[]
+    for i, badge in enumerate(unread_messages):
+        parent_element = badge.find_element(By.XPATH, "ancestor::div[5]")
+        chat_title = parent_element.find_element(By.XPATH, ".//span")
+        if (len(chat_title.text) > 1):
+            logf(f"🔔 {i}.\t{badge.text} messages from ({chat_title.text})")
+            chats.append({"message_count": badge.text, "chat_title": chat_title.text})
+    return jsonify({"status": "success", "chats found": n_chats, "chats": chats})
+
+@app.route('/read', methods=['GET'])
+def read_chat():
+    chat_name = request.args.get("chat_name")
+    n_read = int(request.args.get("n_read", 1))
+    if not chat_name or not n_read:
+        return jsonify({"status": "failure", "error": "Missing chat_name or n_read"}), 400
+    
+    get_chat(chat_name)
+
+    # Wait for the chat's messages to load completely
+    try:
+        chat_window = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'selectable-text')]"))
+        )
+    except Exception as e:
+        logf(f"❌ Failed to load chat window: {e}")
+        driver.quit()
+        return
+    
+    # Now, grab all messages below the 'Unread Messages' tab
+    messages = driver.find_elements(By.XPATH, "//span[contains(@class, 'selectable-text')]")
+    logf(f"Found {len(messages)} messages")
+    found_messages = []
+    if messages:
+        # Display the last 'n_read' messages
+        for i in range(-n_read, 0):
+            last_message = messages[i].text
+            logf(f"📨 Message: {last_message}")
+            found_messages.append(last_message)
+    else:
+        logf(f"❌ No messages found in chat with {chat_name}")
+
+    clear_search()
+
+    return jsonify({"status": "success", "chat": chat_name, "messages": found_messages})
+
+@app.route("/version", methods=['GET'])
+def show_version():
+    return jsonify({"version": version})
+
+def start():
+    global driver 
+    global logfile
+    config = configure()
+    logfile = open(config.get("Settings", "log_path"), 'w')
+    driver = getdriver(config=config, headless=True)  # Start Selenium
+    if driver is not None:
+        app.run(port=config.get('Settings', 'port', fallback=5000))
+    else:
+        logf("Failed to initialize WebDriver")
+    logfile.close()
+
+if __name__ == "__main__":
+    start()
